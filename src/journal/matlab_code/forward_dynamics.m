a24a616c88e2b77ff4646742c8c799c0ff9e4293
JhonPool4 <jhoncharaja@gmail.com>
2021-04-29 02:02:07 -0500

    with variable mass

--- src/journal/matlab_code/forward_dynamics.m
+++ src/journal/matlab_code/forward_dynamics.m
@@ -7,7 +7,7 @@ function [ddq, dq, q, M, b]=forward_dynamics(tau, q, dq, dt)
 % robot parameters
 g  = 9.81; % m/s^2;
 m1 = 10; % kg
-m2 = 50; % kg
+m2 = 10; % kg
 l1 = 1; % kg
 l2 = 1; % kg
 
@@ -21,10 +21,22 @@ b = [2*m2*l2^2*sin(q(2))*cos(q(2))*dq(1)*dq(2) + m2*l1*l2*sin(q(2))*cos(q(1))^2*
 
 % update 
 ddq = inv(M)*(tau - b);
-q   = q  + dt*dq;
 dq  = dq + dt*ddq;
+q   = q  + dt*dq;
+
+% estimated  robot parameters
+g  = 9.81; % m/s^2;
+m1 = 10; % kg
+m2 = 10; % kg
+l1 = 1; % kg
+l2 = 1; % kg
 
 
-% Modificación
-M = 1.0*M;
+% inertia matriz
+M = [ m1*l1^2 + m2*l1^2 + m2*l2^2*sin(q(2))^2,   m2*l1*l2*cos(q(2))*cos(q(1))^2
+     -m2*l1*l2*cos(q(2))*cos(q(1))^2,             m2*l2^2 ];
+
+% nonlinear effects vector 
+b = [2*m2*l2^2*sin(q(2))*cos(q(2))*dq(1)*dq(2) + m2*l1*l2*sin(q(2))*cos(q(1))^2*dq(2)^2
+     2*m2*l1*l2*cos(q(2))*cos(q(1))*sin(q(1))*dq(1)^2 + m2*g*l2*sin(q(2)) ];
 end
