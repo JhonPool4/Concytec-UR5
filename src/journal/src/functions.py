d63c32d366bad6f4e4f0b66ccce69addafe92e7b
EmanuelSamir <samiremp.2@gmail.com>
2021-06-02 00:02:16 -0500

    Added Adam func

--- src/journal/src/functions.py
+++ src/journal/src/functions.py
@@ -606,3 +606,21 @@ def compute_error_dddpose(dddx_des, dddx_act):
     dddx_error[3:7] = -dddx_act[3:7]
     return dddx_error 
 
+class AdamOptimizer:
+    def __init__(self, alpha=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
+        self.alpha = alpha
+        self.beta1 = beta1
+        self.beta2 = beta2
+        self.epsilon = epsilon
+        self.m = 0
+        self.v = 0
+        self.t = 0
+
+    def backward_pass(self, k, gradient):
+        self.t = self.t + 1
+        self.m = self.beta1*self.m + (1 - self.beta1)*gradient
+        self.v = self.beta2*self.v + (1 - self.beta2)*(gradient**2)
+        m_hat = self.m/(1 - self.beta1**self.t)
+        v_hat = self.v/(1 - self.beta2**self.t)
+        k = k - self.alpha*(m_hat/(np.sqrt(v_hat) - self.epsilon))
+        return k
